d6dee615c94b5fcb3804c066332f32cf66cbe1a9
Kokotto3000 <melyssa.auberton@gmail.com>
2021-04-22 20:20:28 +0200

    ajout du menu et modif des data dans index.html, code d'apparition et disparition des images dans le curseur dans main.js

--- js/main.js
+++ js/main.js
@@ -5,11 +5,30 @@ document.addEventListener("DOMContentLoaded", main);
 function main(){
 
     //animation du curseur
-    const cursor = document.querySelector('.cursor');
+    const CURSOR = document.querySelector('.cursor');
 
     document.addEventListener('mousemove', e => {
-        cursor.style.top = `${e.pageY - 30}px`
-        cursor.style.left = `${e.pageX - 30}px`
+        CURSOR.style.top = `${e.pageY - 30}px`
+        CURSOR.style.left = `${e.pageX - 30}px`
     });
+
+    //récupération des liens de la barre de nav
+    const ITEMS= document.querySelectorAll('nav ul li');
+
+    ITEMS.forEach(item => {
+        const LOCATION= item.getAttribute('data-loc');
+
+        //pour faire apparaître les images et grandir le curseur
+        item.addEventListener("mouseenter", ()=> {
+            CURSOR.classList.add('hover');
+            CURSOR.style.backgroundImage= `url(../ressources/${LOCATION}.jpg)`;
+        });
+
+        //le curseur rétrécit et l'image disparaît
+        item.addEventListener("mouseout", ()=> {
+            CURSOR.classList.remove('hover');
+            CURSOR.style.backgroundImage= "none";
+        });
+    })
 
 }
